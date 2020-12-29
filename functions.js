306bd8aa1da4ce9b4b14b8a9adb61217c93f8ad1
Waqas Khan <wuikhan@gmail.com>
2020-12-29 16:55:52 -0500

    object literals

--- functions.js
+++ functions.js
@@ -685,3 +685,236 @@ function Product(id, name, number) {
     this.standardCost = 10;
     this.listPrice = 30;
 }
+
+// Understanding this
+
+// Global scope - 'this' is mapped to global/window object
+console.log("Begin: global scope sample");
+console.log(this.toString());
+console.log("this === window = " + (this === window).toString());
+console.log("End: global scope sample");
+
+// Function scope - 'this' is mapped to global/window object
+//                  Uncomment 'use strict' above to show how it affects this function
+function functionScope() {
+    console.log(this.toString());
+    console.log("this === window = " + (this === window).toString());
+}
+
+// Object literal
+function objectLiteral() {
+    let product = {
+        "productID": 680,
+        "name": "HL Road Frame - Black, 58",
+        "standardCost": 1059.31,
+        "listPrice": 1431.50,
+        grossProfit: function () {
+            return (this.listPrice - this.standardCost)
+                .toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD'
+                });
+        }
+    };
+
+    console.log(product.grossProfit());
+}
+
+
+function callAndApply() {
+    let product = {
+        "productID": 680,
+        "name": "HL Road Frame - Black, 58",
+        "standardCost": 1059.31,
+        "listPrice": 1431.50,
+        grossProfit: function () {
+            return (this.listPrice - this.standardCost)
+                .toLocaleString("en-US", {
+                    "style": "currency",
+                    "currency": "USD"
+                });
+        }
+    };
+
+    let prod2 = {
+        "standardCost": 500,
+        "listPrice": 850
+    }
+
+    // Call using reference to 'product' properties
+    console.log(product.grossProfit.call(product));
+    // Call using reference to 'prod2' properties
+    console.log(product.grossProfit.call(prod2));
+    console.log("");
+    console.log(product.grossProfit.apply(product));
+    console.log(product.grossProfit.apply(prod2));
+}
+
+function constructorFunctions() {
+    let prod1 = new Product(680, "HL Road Frame - Black, 58", 1059.31, 1431.50);
+    let prod2 = new Product(707, "Sport-100 Helmet, Red", 13.08, 34.99);
+
+    console.log(prod1.grossProfit());
+    console.log(prod2.grossProfit());
+}
+
+function Product(id, name, cost, price) {
+    this.productID = id;
+    this.name = name;
+    this.standardCost = cost;
+    this.listPrice = price;
+
+    this.grossProfit = function () {
+        return (this.listPrice - this.standardCost)
+            .toLocaleString("en-US", {
+                "style": "currency",
+                "currency": "USD"
+            });
+    }
+}
+
+// String to array
+function stringToArray() {
+    let productNumber = "FR-R92B-58";
+
+    let values = [...productNumber];
+
+    console.log(values);
+}
+// Filter out duplicates from array
+function filterDupes() {
+    let numbers = [680, 710, 680, 900, 710];
+
+    console.log(...new Set(numbers));
+}
+
+// Copy array
+function copyArray() {
+    let arr = [1, 2, 3];
+    let arr2 = [...arr];
+
+    // Make changes to duplicated array
+    arr2.push(4);
+    arr2[0] = 99;
+
+    console.log(arr);
+    console.log(arr2);
+}
+
+
+let _productss = [{
+        productID: 680,
+        name: "HL Road Frame - Black, 58",
+        productNumber: "FR-R92B-58",
+        color: "Black",
+        standardCost: 1059.31,
+        listPrice: 1431.50
+    },
+    {
+        productID: 707,
+        name: "Sport-100 Helmet, Red",
+        productNumber: "HL-U509-R",
+        color: "Red",
+        standardCost: 13.08,
+        listPrice: 34.99
+    },
+    {
+        productID: 709,
+        name: "Mountain Bike Socks, M",
+        productNumber: "SO-B909-M",
+        color: "White",
+        standardCost: 3.3963,
+        listPrice: 9.50
+    }
+];
+
+let _newProducts = [{
+        productID: 712,
+        name: "AWC Logo Cap",
+        productNumber: "CA-1098",
+        color: "Multi",
+        standardCost: 6.9223,
+        listPrice: 8.99
+    },
+    {
+        productID: 821,
+        name: "Touring Front Wheel",
+        productNumber: "FW-T905",
+        color: "Black",
+        standardCost: 96.7964,
+        listPrice: 218.01
+    }
+];
+
+
+function copyObjectArrays() {
+    // Careful with object arrays
+    // The array is copied, but the underlying objects are still accessed by reference
+    let diff = [..._productss];
+
+    diff[0].productID = 999;
+    console.log(_productss[0].productID);
+    console.log(diff[0].productID);
+}
+
+
+// Concatentation
+function concatenateArray() {
+    let allProducts = _products.concat(_newProducts);
+    console.log(allProducts.length);
+
+    let spProducts = [..._products, ..._newProducts];
+    console.log(spProducts.length);
+}
+
+// Use with 'new'
+function spreadInConstructors() {
+    let dt = new Date(2019, 10, 15); // 15 Nov 2019
+    console.log(dt);
+
+    let dateFields = [2019, 11, 15]; // 15 Dec 2019
+    dt = new Date(...dateFields);
+    console.log(dt);
+}
+
+function spreadForFunctionArgs() {
+    multipleParams(1, 2, 3);
+
+    let args = [1, 2, 3];
+    multipleParams(...args);
+}
+
+function multipleParams(arg1, arg2, arg3) {
+    console.log(arg1);
+    console.log(arg2);
+    console.log(arg3);
+    console.log("");
+}
+
+// Use with object literals
+function objectLiterals() {
+    let product = {
+        productID: 680,
+        name: "HL Road Frame - Black, 58",
+        standardCost: 1059.31,
+        listPrice: 1431.50
+    };
+
+    // The following performs a shallow-copy
+    // Similar to Object.assign()
+    let prod2 = {
+        ...product
+    };
+
+    // Change the newly copied object
+    prod2.productID = 999;
+
+    // Display the objects
+    console.log(product);
+    console.log(prod2);
+
+    // Display the changed value
+    console.log("");
+    console.log(product.productID);
+    console.log(prod2.productID);
+}
